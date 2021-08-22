ea7b1a8a7c1d7f737ee6893a735b2efd0eb2308f
next-fire-starter <next.fire.starter@gmail.com>
2021-08-22 22:53:16 +0900

    Update create-table.sql

--- DB/create-table.sql
+++ DB/create-table.sql
@@ -1,4 +1,27 @@
+---------------------------------------------------------------------------------------------postgresql
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  id serial,
+  name VARCHAR(60) NOT NULL,
+  email VARCHAR(120) NOT NULL,
+  password VARCHAR(120) NOT NULL,
+  roles VARCHAR(120),
+  lock_flag BOOLEAN NOT NULL DEFAULT FALSE,
+  disable_flag BOOLEAN NOT NULL DEFAULT FALSE,
+  create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id)
+);
 
+COMMENT ON COLUMN users.id IS 'ユーザID';
+COMMENT ON COLUMN users.name IS 'ユーザー名';
+COMMENT ON COLUMN users.email IS 'メールアドレス';
+COMMENT ON COLUMN users.password IS 'パスワード';
+COMMENT ON COLUMN users.roles IS 'ロール';
+COMMENT ON COLUMN users.lock_flag IS 'ロックフラグ TRUE:ロック';
+COMMENT ON COLUMN users.disable_flag IS '無効フラグ TRUE:無効';
+COMMENT ON COLUMN users.create_at IS '作成日';
+COMMENT ON COLUMN users.update_at IS '更新日';
 ---------------------------------------------------------------------------------------------mysql
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
